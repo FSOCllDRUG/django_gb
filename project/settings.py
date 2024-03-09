9de1a14a37de1292ff196319626446f26fc6ee9a
Vlad <vlad.a.borshch@gmail.com>
2024-03-09 18:04:36 +0300

    edited settings.py

--- project/settings.py
+++ project/settings.py
@@ -36,10 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #'my1app',
-    #'Authorapp',
     'Orderapp',
-    #'debug_toolbar',
 ]
 
 MIDDLEWARE = [
